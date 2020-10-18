7c159e8c2e1721d92749ee9e793fd1326ee260bd
Lê Anh Tú <anhtu17698@outlook.com>
2020-10-19 00:14:40 +0700

    finished Index Create employee

---
+++ GaraManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GaraManagement.Models;
+
+namespace GaraManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly GaraContext _context;
+
+        public EmployeesController(GaraContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        [HttpGet]
+        public IActionResult Create(string layout = "_")
+        {
+            ViewData["Layout"] = layout == "_" ? "" : layout;
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,DateOfBirth,ContractStartDate,Salary,Phone,Address,IdentityCardNumber,Department")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,DateOfBirth,ContractStartDate,Salary,Phone,Address,IdentityCardNumber,Department")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
--- GaraManagement/Models/Employee.cs
+++ GaraManagement/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,13 +15,23 @@ namespace GaraManagement.Models
         }
 
         public string Id { get; set; }
+        [DisplayName("Tên")]
         public string Name { get; set; }
+        [DisplayName("Ngày sinh")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime? DateOfBirth { get; set; }
+        [DisplayName("Ngày BĐ hợp đồng")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime? ContractStartDate { get; set; }
+        [DisplayName("Lương")]
         public int? Salary { get; set; }
+        [DisplayName("Điện thoại")]
         public string Phone { get; set; }
+        [DisplayName("Địa chỉ")]
         public string Address { get; set; }
+        [DisplayName("CMND")]
         public string IdentityCardNumber { get; set; }
+        [DisplayName("Bộ phận")]
         public string Department { get; set; }
 
         public virtual ICollection<Account> Accounts { get; set; }
