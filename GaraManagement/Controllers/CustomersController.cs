d8d324955ac142b7421b041b95d3db74b557c9af
Lê Anh Tú <anhtu17698@outlook.com>
2020-10-27 15:19:07 +0700

    finished Customer

---
+++ GaraManagement/Controllers/CustomersController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GaraManagement.Models;
+using X.PagedList;
+
+namespace GaraManagement.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly GaraContext _context;
+
+        public CustomersController(GaraContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public  IActionResult Index(string search,int? pageNumber)
+        {
+            if (pageNumber == null) pageNumber = 1;
+            int pageSize = 10;
+            ViewData["GetTextSearch"] = search;
+            if (!string.IsNullOrEmpty(search))
+            {
+                var garaContext = _context.Customers.Where(a => a.Name.Contains(search));
+                return View(garaContext.ToList().ToPagedList((int)pageNumber, pageSize));
+            }
+            else
+            {
+                var garaContext = _context.Customers;
+                return View(garaContext.ToList().ToPagedList((int)pageNumber, pageSize));
+            }
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create(string layout = "_")
+        {
+            ViewData["Layout"] = layout == "_" ? "" : layout;
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,DateOfBirth,Gender,Phone,Address,IdentityCardNumber")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(string id,string layout = "_")
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ViewData["Layout"] = layout == "_" ? "" : layout;
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,DateOfBirth,Gender,Phone,Address,IdentityCardNumber")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(string id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
