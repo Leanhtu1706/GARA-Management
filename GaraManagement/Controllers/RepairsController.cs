b264dc441cd68b401e11b380897a5d8fac741a82
Lê Anh Tú <anhtu17698@outlook.com>
2021-03-16 22:57:45 +0700

    Complete Export BaoGia to Excel

--- GaraManagement/Controllers/RepairsController.cs
+++ GaraManagement/Controllers/RepairsController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GaraManagement.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace GaraManagement.Controllers
 {
@@ -114,11 +117,11 @@ namespace GaraManagement.Controllers
         public IActionResult Create(int idCar, string layout = "_")
         {
             var existsRepair = _context.Repairs.Where(r => r.IdCar == idCar && r.State != StateType.completed).FirstOrDefault();
-            if(existsRepair != null)
+            if (existsRepair != null)
             {
                 HttpContext.Session.SetString("ErrorMessage", "Xe này đang ở trong xưởng");
                 return Json("false");
-            }    
+            }
             ViewData["Layout"] = layout == "_" ? "" : layout;
             ViewData["IdCar"] = idCar;
             return View();
@@ -225,7 +228,7 @@ namespace GaraManagement.Controllers
         {
             var repair = await _context.Repairs.FindAsync(id);
             var goodsDeliveryNote = _context.GoodsDeliveryNotes.Where(g => g.IdRepair == id).FirstOrDefault();
-            if(goodsDeliveryNote != null)
+            if (goodsDeliveryNote != null)
             {
                 var detailGoodsDeliveryNote = _context.DetailGoodsDeliveryNotes.Include(dg => dg.IdGoodsDeliveryNoteNavigation).Where(dg => dg.IdGoodsDeliveryNote == goodsDeliveryNote.Id).ToList();
                 if (detailGoodsDeliveryNote.Any())
@@ -233,7 +236,7 @@ namespace GaraManagement.Controllers
                     foreach (var itemdg in detailGoodsDeliveryNote)
                     {
                         _context.DetailGoodsDeliveryNotes.Remove(itemdg);
-                    } 
+                    }
                 }
                 _context.GoodsDeliveryNotes.Remove(goodsDeliveryNote);
             }
@@ -245,7 +248,7 @@ namespace GaraManagement.Controllers
                     _context.DetailRepairs.Remove(itemdr);
                 }
             }
-           
+
             _context.Repairs.Remove(repair);
             await _context.SaveChangesAsync();
             HttpContext.Session.SetString("SuccessMessage", "Xóa thành công");
@@ -265,7 +268,7 @@ namespace GaraManagement.Controllers
                 .Include(r => r.GoodsDeliveryNotes)
                 .ThenInclude(r => r.DetailGoodsDeliveryNotes)
                 .ThenInclude(r => r.IdMaterialNavigation)
-                .ThenInclude(r=>r.PriceMaterials)
+                .ThenInclude(r => r.PriceMaterials)
                 .Include(r => r.DetailRepairs)
                 .ThenInclude(r => r.IdWorkNavigation)
                 .Include(r => r.Pays)
@@ -275,6 +278,166 @@ namespace GaraManagement.Controllers
                 ViewData["exists"] = "exists";
             }
             return View(repair);
+        }
+
+        public async Task<IActionResult> Export(int id)
+        {
+            var repair = await _context.Repairs
+                .Include(r => r.IdCarNavigation)  
+                .ThenInclude(r => r.IdCustomerNavigation)
+                .Include(r => r.GoodsDeliveryNotes)
+                .ThenInclude(r => r.DetailGoodsDeliveryNotes)
+                .ThenInclude(r => r.IdMaterialNavigation)
+                .ThenInclude(r => r.PriceMaterials)
+                .Include(r => r.DetailRepairs)
+                .ThenInclude(r => r.IdWorkNavigation)
+                .Include(r => r.Pays)
+                .Include(r => r.IdCarNavigation.IdCarModelNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+
+            var stream = new MemoryStream();
+
+            using (var package = new ExcelPackage(stream))
+            {
+                var sheet = package.Workbook.Worksheets.Add("Báo giá");
+                
+
+                sheet.Cells["C2"].Value = "Báo giá sửa chữa";
+
+                sheet.Cells["A4"].Value = "Tên công ty:";
+                sheet.Cells["A5"].Value = "Mã Số thuế:";
+                sheet.Cells["A6"].Value = "Mã phiếu sửa:   " + repair.Id;
+                sheet.Cells["A7"].Value = "Tên Khách hàng:   "+ repair.IdCarNavigation.IdCustomerNavigation.Name;
+                sheet.Cells["A8"].Value = "Số điện thoại:   " + repair.IdCarNavigation.IdCustomerNavigation.Phone; 
+                sheet.Cells["A9"].Value = "Địa chỉ:   " + repair.IdCarNavigation.IdCustomerNavigation.Address;
+                sheet.Cells["A10"].Value = "Số CMND:   " + repair.IdCarNavigation.IdCustomerNavigation.IdentityCardNumber; 
+
+                sheet.Cells["D6"].Value = "Thời gian:" + DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                sheet.Cells["D7"].Value = "Biển số:"; sheet.Cells["E7"].Value = repair.IdCarNavigation.LicensePlates;
+                sheet.Cells["D8"].Value = "Dòng xe:"; sheet.Cells["E8"].Value = repair.IdCarNavigation.IdCarModelNavigation.ModelName;
+                sheet.Cells["D9"].Value = "Màu:"; sheet.Cells["E9"].Value = repair.IdCarNavigation.Color;
+                sheet.Cells["D10"].Value = "Ghi chú:"; sheet.Cells["E10"].Value = repair.IdCarNavigation.Note;
+                sheet.Cells["A11"].Value = "A. Chi tiết tiền vật tư:";
+                sheet.Cells["A12"].Value = "Tên vật tư";
+                sheet.Cells["B12"].Value = "Đv tính";
+                sheet.Cells["C12"].Value = "Số lượng";
+                sheet.Cells["D12"].Value = "Đơn giá";
+                sheet.Cells["E12"].Value = "thành tiền";
+
+                using (ExcelRange exr = sheet.Cells[7,1,7,5])
+                {
+                    exr.Style.Border.Top.Style = ExcelBorderStyle.Thin;  
+                }
+                using (ExcelRange exr = sheet.Cells[10,1,10,5])
+                {
+                    exr.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;  
+                }
+                using (ExcelRange exr = sheet.Cells[7,5,10,5])
+                {
+                    exr.Style.Border.Right.Style = ExcelBorderStyle.Thin;  
+                }
+                using (ExcelRange exr = sheet.Cells[7,1,10,1])
+                {
+                    exr.Style.Border.Left.Style = ExcelBorderStyle.Thin;  
+                }
+
+                //Set style
+                sheet.Cells["C2,D6,A4,A5,A6,A11,A12,B12,C12,D12,E12"].Style.Font.Bold = true;
+                sheet.Cells["C2"].Style.Font.Size = 14;
+                
+                //định dạng money
+                var info = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");
+                //Show danh sách vật tư
+                var rowNumber = 13;
+                int? tongTienVatTu = 0;
+                foreach (var item in repair.GoodsDeliveryNotes.Where(r => r.IdRepair == id).FirstOrDefault().DetailGoodsDeliveryNotes)
+                {
+                    sheet.Cells["A" + rowNumber].Value = item.IdMaterialNavigation.Name;
+                    sheet.Cells["B" + rowNumber].Value = item.IdMaterialNavigation.Unit;
+                    sheet.Cells["C" + rowNumber].Value = item.Amount;
+                    sheet.Cells["D" + rowNumber].Value = String.Format(info, "{0:c}", item.Price);
+                    sheet.Cells["E" + rowNumber].Value = String.Format(info, "{0:c}", item.Amount * item.Price);
+                    tongTienVatTu += item.Amount * item.Price;
+                    rowNumber++;
+                }
+                using (ExcelRange exr = sheet.Cells[12, 1, rowNumber-1, 5])
+                {
+                    //exr.AutoFitColumns();
+                    exr.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                }
+                // show danh sách tiền công
+                var rowNumberWork = rowNumber + 2;
+                int? tongTienCong = 0;
+                sheet.Cells["A"+ rowNumber].Value = "B. Chi tiết tiền công:";
+                sheet.Cells["A" + (rowNumber + 1)].Value = "Tên công việc";
+                sheet.Cells["B" + (rowNumber + 1)].Value = "Số lượng";
+                sheet.Cells["C" + (rowNumber + 1)].Value = "Chi phí";
+                sheet.Cells["D" + (rowNumber + 1)].Value = "thành tiền";
+                sheet.Cells[rowNumber + 1, 4, rowNumber + 1, 5].Merge = true;
+
+                sheet.Cells["A" + rowNumber].Style.Font.Bold = true;
+                sheet.Cells["A" + (rowNumber + 1)].Style.Font.Bold = true;
+                sheet.Cells["B" + (rowNumber + 1)].Style.Font.Bold = true;
+                sheet.Cells["C" + (rowNumber + 1)].Style.Font.Bold = true;
+                sheet.Cells["D" + (rowNumber + 1)].Style.Font.Bold = true;
+                foreach (var item in repair.DetailRepairs)
+                {
+                    sheet.Cells[rowNumberWork, 4, rowNumberWork, 5].Merge = true;
+                    sheet.Cells["A" + rowNumberWork].Value = item.IdWorkNavigation.WorkName;
+                    sheet.Cells["B" + rowNumberWork].Value = item.Amount;
+                    sheet.Cells["C" + rowNumberWork].Value = String.Format(info, "{0:c}", item.IdWorkNavigation.Cost);
+                    sheet.Cells["D" + rowNumberWork].Value = String.Format(info, "{0:c}", item.Amount * item.IdWorkNavigation.Cost);
+                    tongTienCong += item.Amount * item.IdWorkNavigation.Cost;
+                    rowNumberWork++;    
+                }
+                using (ExcelRange exr = sheet.Cells[rowNumber + 1, 1, rowNumberWork - 1, 5])
+                {
+                    //exr.AutoFitColumns();
+                    exr.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    exr.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                }
+
+                sheet.Cells["A4:E11"].AutoFitColumns();
+                sheet.Cells["A11:E100"].AutoFitColumns();
+                sheet.Cells["A11:E100"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                sheet.Cells["A11"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                sheet.Cells["A" + rowNumber].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+
+                sheet.Cells["A" + (rowNumberWork + 1)].Value = "Cộng:  " + String.Format(info, "{0:c}", (tongTienCong + tongTienVatTu));
+                sheet.Cells["A" + (rowNumberWork + 2)].Value = "Thuế VAT(10%):  " + String.Format(info, "{0:c}", (((tongTienCong + tongTienVatTu) * 10) / 100));
+                sheet.Cells["A" + (rowNumberWork + 3)].Value = "Tổng:  " + String.Format(info, "{0:c}", ((tongTienCong + tongTienVatTu) + (((tongTienCong + tongTienVatTu) * 10) / 100)));
+                sheet.Cells["A" + (rowNumberWork + 1)].Style.Font.Bold = true;
+                sheet.Cells["A" + (rowNumberWork + 2)].Style.Font.Bold = true;
+                sheet.Cells["A" + (rowNumberWork + 3)].Style.Font.Bold = true;
+                sheet.Cells["A" + (rowNumberWork + 5)].Style.Font.Bold = true;
+                sheet.Cells["D" + (rowNumberWork + 5)].Style.Font.Bold = true;
+
+                using (ExcelRange exr = sheet.Cells[rowNumberWork + 5, 1, rowNumberWork + 5, 5])
+                {
+                    exr.Style.Border.Top.Style = ExcelBorderStyle.Thick;
+                    
+
+                }
+                sheet.Cells["A" + (rowNumberWork + 5)].Value = "Cố vấn dịch vụ";
+                sheet.Cells["D" + (rowNumberWork + 5)].Value = "Khách hàng";
+                sheet.Cells["A" + (rowNumberWork + 6)].Value = sheet.Cells["D" + (rowNumberWork + 6)].Value = "(Ký ghi rõ họ tên)";
+
+
+
+                package.Save();
+            }
+
+            stream.Position = 0;
+            var fileName = $"BaoGia_{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
         }
     }
 }
